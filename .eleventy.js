94241d36e2e925e4b10048975f6b285b9bbdb114
Spencer Harston <spencer314@gmail.com>
2022-04-09 00:18:37 -0600

    config: add filter for pretty printing dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon');
+
 module.exports = function(eleventyConfig){
 
     eleventyConfig.setDataDeepMerge(true);
@@ -5,6 +7,11 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy({
         'src/static': 'static/',
         'src/favicon.ico': '/favicon.ico'
+    })
+
+    // filter to return dates as pretty string, like April 1, 2022
+    eleventyConfig.addFilter('prettyDate', (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     })
 
     return {
