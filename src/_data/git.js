f0ab0e42581ea118ee5bd975a805339c1632437c
Spencer Harston <spencer314@gmail.com>
2022-04-09 23:05:10 -0600

    fix: one more try to build

--- src/_data/git.js
+++ src/_data/git.js
@@ -5,7 +5,6 @@ module.exports = async function () {
         commit_sha: await git.short,
         curr_branch: await git.branch,
         commit_date: await git.date,
-        origin: await git.origin,
         repo: "spencerharston.com"
     }
 
