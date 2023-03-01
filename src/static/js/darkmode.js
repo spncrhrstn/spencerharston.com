380833731b7184e4cab01aee136a8f6c29adcf4b
Spencer Harston <spencer314@gmail.com>
2023-03-01 09:54:58 -0700

    feat: add dark mode toggle

--- src/static/js/darkmode.js
+++ src/static/js/darkmode.js
@@ -1,20 +1,34 @@
+function isDarkModeEnabled() {
+  return localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+}
+
 const darkModeButton = document.getElementById('dark-mode-button');
 darkModeButton.addEventListener('click', (e) => {
-    console.log("dark mode toggled");
+  if(isDarkModeEnabled()){
+    // disable it
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
+    console.log("dark mode disabled");
+  } else{
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+    console.log("dark mode enabled");
+  }
+
 })
 
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
-  
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
+if (isDarkModeEnabled()) {
+  document.documentElement.classList.add('dark')
+} else {
+  document.documentElement.classList.remove('dark')
+}
+
+// // Whenever the user explicitly chooses light mode
+// localStorage.theme = 'light'
+
+// // Whenever the user explicitly chooses dark mode
+// localStorage.theme = 'dark'
+
+// // Whenever the user explicitly chooses to respect the OS preference
+// localStorage.removeItem('theme')
