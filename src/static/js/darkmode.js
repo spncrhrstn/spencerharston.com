bd95ec8174b07d658471c21f8b14b664b4701365
Spencer Harston <spencer314@gmail.com>
2023-03-01 10:04:29 -0700

    feat: switch the dark mode button icon per mode

--- src/static/js/darkmode.js
+++ src/static/js/darkmode.js
@@ -8,10 +8,12 @@ darkModeButton.addEventListener('click', (e) => {
     // disable it
     document.documentElement.classList.remove('dark');
     localStorage.theme = 'light';
+    darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'moon'}" /></svg>`
     console.log("dark mode disabled");
   } else{
     document.documentElement.classList.add('dark');
     localStorage.theme = 'dark';
+    darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'sun'}" /></svg>`
     console.log("dark mode enabled");
   }
 
@@ -20,8 +22,10 @@ darkModeButton.addEventListener('click', (e) => {
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (isDarkModeEnabled()) {
   document.documentElement.classList.add('dark')
+  darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'sun'}" /></svg>`
 } else {
   document.documentElement.classList.remove('dark')
+  darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'moon'}" /></svg>`
 }
 
 // // Whenever the user explicitly chooses light mode
