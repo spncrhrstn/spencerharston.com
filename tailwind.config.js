a74b2f181f85018686930e481904e866330bd207
Spencer Harston <spencer314@gmail.com>
2023-03-01 13:03:57 -0700

    style: lint fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,40 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,njk}"],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
     extend: {
       aria: {
-        current: 'current=page'
+        current: "current=page"
       },
       fontFamily: {
-        'sans':['"Public Sans"', ...defaultTheme.fontFamily.sans],
-        'mono':['"JetBrains Mono"', ...defaultTheme.fontFamily.mono]
+        "sans":["\"Public Sans\"", ...defaultTheme.fontFamily.sans],
+        "mono":["\"JetBrains Mono\"", ...defaultTheme.fontFamily.mono]
       },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            color: theme('colors.zinc.800'),
+            color: theme("colors.zinc.800"),
             a: {
-              color: theme('colors.sky.900'),
-              textDecoration: 'none',
-              '&:hover' : {
-                textDecoration: theme('underline')
+              color: theme("colors.sky.900"),
+              textDecoration: "none",
+              "&:hover" : {
+                textDecoration: theme("underline")
               }
             },
-            'ul ul, ul ol, ol ul, ol ol': {
-              marginTop: '0',
-              marginBottom: '0'
+            "ul ul, ul ol, ol ul, ol ol": {
+              marginTop: "0",
+              marginBottom: "0"
             }
           }
         },
         invert: {
           css: {
-            color: theme('colors.zinc.200'),
+            color: theme("colors.zinc.200"),
             a: {
-              color: theme('colors.blue.400'),
+              color: theme("colors.blue.400"),
             }
           }
         }
@@ -42,6 +42,6 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/typography')
+    require("@tailwindcss/typography")
   ],
-}
+};
