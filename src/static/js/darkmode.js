1afababc5a4987b021e916964f9aa2e696fbfea0
Spencer Harston <spencer314@gmail.com>
2023-02-28 22:45:08 -0700

    feat: add dark mode toggle button

--- src/static/js/darkmode.js
+++ src/static/js/darkmode.js
@@ -1,3 +1,8 @@
+const darkModeButton = document.getElementById('dark-mode-button');
+darkModeButton.addEventListener('click', (e) => {
+    console.log("dark mode toggled");
+})
+
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
     document.documentElement.classList.add('dark')
