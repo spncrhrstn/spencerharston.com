4d0435f91ede4e3332bfb13826d42069460da87e
Spencer Harston <spencer314@gmail.com>
2023-03-01 13:02:40 -0700

    fix: darkmode script changes

--- src/static/js/darkmode.js
+++ src/static/js/darkmode.js
@@ -1,38 +1,35 @@
 function isDarkModeEnabled() {
-  return localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches);
+  return localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches);
 }
 
-const darkModeButton = document.getElementById('dark-mode-button');
-darkModeButton.addEventListener('click', (e) => {
+function enableDarkMode(set) {
+  if(set){
+    document.documentElement.classList.add("dark");
+    darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${"sun"}" /></svg>`;
+    console.log("( •_•)>⌐■-■");
+  } else {
+    document.documentElement.classList.remove("dark");
+    darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${"moon"}" /></svg>`;
+    console.log("(⌐■_■)");  
+  }
+}
+
+const darkModeButton = document.getElementById("dark-mode-button");
+darkModeButton.addEventListener("click", () => {
+  // handle user explicitly enable/disable theme
   if(isDarkModeEnabled()){
     // disable it
-    document.documentElement.classList.remove('dark');
-    localStorage.theme = 'light';
-    darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'moon'}" /></svg>`
-    console.log("dark mode disabled");
+    enableDarkMode(false);
+    localStorage.theme = "light";
   } else{
-    document.documentElement.classList.add('dark');
-    localStorage.theme = 'dark';
-    darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'sun'}" /></svg>`
-    console.log("dark mode enabled");
+    enableDarkMode(true);
+    localStorage.theme = "dark";
   }
-
-})
+});
 
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 if (isDarkModeEnabled()) {
-  document.documentElement.classList.add('dark')
-  darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'sun'}" /></svg>`
+  enableDarkMode(true);
 } else {
-  document.documentElement.classList.remove('dark')
-  darkModeButton.innerHTML = `<svg class="feather" width="${16}" height="${16}"><use href="/static/img/icons/feather-sprite.svg#${'moon'}" /></svg>`
+  enableDarkMode(false);
 }
-
-// // Whenever the user explicitly chooses light mode
-// localStorage.theme = 'light'
-
-// // Whenever the user explicitly chooses dark mode
-// localStorage.theme = 'dark'
-
-// // Whenever the user explicitly chooses to respect the OS preference
-// localStorage.removeItem('theme')
