17b877feb44e59d563ac6c6346d8cb107569097b
Spencer Harston <spencer314@gmail.com>
2025-01-16 00:00:35 -0700

    collections: make sort functions a bit more generic

--- config/filters/collections.js
+++ config/filters/collections.js
@@ -1,6 +1,16 @@
 import { DateTime } from "luxon";
 
-const sortDesc = (collection) => collection.sort((a, b) => b.date - a.date);
+const sortByKey = (collection, key, dir = "asc") => {
+  if (dir === "asc") return collection.sort((a, b) => a[key].localeCompare(b[key]));
+  if (dir === "desc") return collection.sort((a, b) => b[key].localeCompare(a[key]));
+  else return collection;
+};
+
+const sortDateByKey = (collection, key = "date", dir = "asc") => {
+  if (dir === "asc") return collection.sort((a, b) => new Date(a[key]) - new Date(b[key]));
+  if (dir === "desc") return collection.sort((a, b) => new Date(b[key]) - new Date(a[key]));
+  else return collection;
+};
 
 const limit = (collection, limit) => collection.slice(0, limit);
 
@@ -10,7 +20,8 @@ const postsByYear = (collection, year) =>
   collection.filter((entry) => DateTime.fromJSDate(entry.date).year.toString() === year);
 
 export default {
-  sortDesc,
+  sortByKey,
+  sortDateByKey,
   limit,
   postsByTag,
   postsByYear
